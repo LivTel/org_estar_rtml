d18fb62185a015903bb1eaad696d1dcaf76dc8ca
Jason Etherton <je@example.com>
2004-03-12 10:55:18 +0000

    Initial revision

---
+++ org_estar_rtml/RTMLContact.java
@@ -0,0 +1,281 @@
+package org.estar.rtml;
+
+import java.net.URL;
+
+/**
+ * This class is a data container for information contained in the Contact
+ * nodes/tags of an RTML document.
+ * @author Jason Etherton
+ * @version $Revision: 1.1 $
+ */
+public class RTMLContact
+{
+  /**
+   * Revision control system version id.
+   */
+  public final static String RCSID = "";
+
+  /**
+   * The User element for this Contact.
+   */
+  private String user = null;
+
+  /**
+   * The Name element for this Contact.
+   */
+  private String name = null;
+
+  /**
+   * The Institution element for this Contact.
+   */
+  private String institution = null;
+
+  /**
+   * The Address element for this Contact.
+   */
+  private String address = null;
+
+  /**
+   * The Telephone element for this Contact.
+   */
+  private String telephone = null;
+
+  /**
+   * The Fax element for this Contact.
+   */
+  private String fax = null;
+
+  /**
+   * The Email element for this Contact.
+   */
+  private String email = null;
+
+  /**
+   * The Url element for this Contact.
+   */
+  private URL url = null;
+
+
+  /**
+   * Default constructor.
+   */
+  public RTMLContact()
+  {
+    super();
+  }
+
+  /**
+   * Method to set the User.
+   * @param newUser the new User to set
+   */
+  public void setUser( String newUser )
+  {
+    user = newUser;
+  }
+
+
+  /**
+   * Method to get the current User.
+   * @return User
+   * @see #user
+   */
+  public String getUser()
+  {
+    return( user );
+  }
+
+
+  /**
+   * Method to set the Name.
+   * @param newName the new Name to set
+   */
+  public void setName( String newName )
+  {
+    name = newName;
+  }
+
+
+  /**
+   * Method to get the current Name.
+   * @return Name
+   * @see #name
+   */
+  public String getName()
+  {
+    return( name );
+  }
+
+
+  /**
+   * Method to set the Institution.
+   * @param newInstitution the new Institution to set
+   */
+  public void setInstitution( String newInstitution )
+  {
+    institution = newInstitution;
+  }
+
+
+  /**
+   * Method to get the current Institution.
+   * @return Institution
+   * @see #institution
+   */
+  public String getInstitution()
+  {
+    return( institution );
+  }
+
+
+  /**
+   * Method to set the Address.
+   * @param newAddress the new Address to set
+   */
+  public void setAddress( String newAddress )
+  {
+    address = newAddress;
+  }
+
+
+  /**
+   * Method to get the current Address.
+   * @return Address
+   * @see #address
+   */
+  public String getAddress()
+  {
+    return( address );
+  }
+
+
+  /**
+   * Method to set the Telephone.
+   * @param newTelephone the new Telephone to set
+   */
+  public void setTelephone( String newTelephone )
+  {
+    telephone = newTelephone;
+  }
+
+
+  /**
+   * Method to get the current Telephone.
+   * @return Telephone
+   * @see #telephone
+   */
+  public String getTelephone()
+  {
+    return( telephone );
+  }
+
+
+  /**
+   * Method to set the Fax.
+   * @param newFax the new Fax to set
+   */
+  public void setFax( String newFax )
+  {
+    fax = newFax;
+  }
+
+
+  /**
+   * Method to get the current Fax.
+   * @return Fax
+   * @see #fax
+   */
+  public String getFax()
+  {
+    return( fax );
+  }
+
+
+  /**
+   * Method to set the Email.
+   * @param newEmail the new Email to set
+   */
+  public void setEmail( String newEmail )
+  {
+    email = newEmail;
+  }
+
+
+  /**
+   * Method to get the current Email.
+   * @return Email
+   * @see #email
+   */
+  public String getEmail()
+  {
+    return( email );
+  }
+
+
+  /**
+   * Method to set the Url.
+   * @param newUrl the new Url to set
+   */
+  public void setUrl( URL newUrl )
+  {
+    url = newUrl;
+  }
+
+
+  /**
+   * Method to get the current Url.
+   * @return Url
+   * @see #Url
+   */
+  public URL getUrl()
+  {
+    return( url );
+  }
+
+
+  /**
+   * String representation of this Conatct.
+   * 'see #toString(java.lang.String)
+   */
+  public String toString()
+  {
+    return( toString( "" ) );
+  }
+
+
+  /**
+   * String representation of this Conatct, with specified prefix.
+   * @param prefix A string to prefix to each line of data we print out.
+   * @see #getName
+   * @see #getUser
+   * @see #getInstitution
+   * @see #getAddress
+   * @see #getTelephone
+   * @see #getFax
+   * @see #getEmail
+   * @see #getUrl
+   */
+  public String toString( String prefix )
+  {
+    StringBuffer sb = new StringBuffer();
+
+    sb.append( prefix+"Contact :\n" );
+    sb.append( prefix+"\t       Name : "+name+"\n" );
+    sb.append( prefix+"\t       User : "+user+"\n" );
+    sb.append( prefix+"\tInstitution : "+institution+"\n" );
+    sb.append( prefix+"\t    Address : "+address+"\n" );
+    sb.append( prefix+"\t  Telephone : "+telephone+"\n" );
+    sb.append( prefix+"\t        Fax : "+fax+"\n" );
+    sb.append( prefix+"\t      eMail : "+email+"\n" );
+    sb.append( prefix+"\t        URL : "+url+"\n" );
+
+    return( sb.toString() );
+  }
+}
+/*
+ *    $Date: 2004-03-12 10:55:18 $
+ * $RCSfile: RTMLContact.java,v $
+ *  $Source: /space/home/eng/cjm/cvs/org_estar_rtml/RTMLContact.java,v $
+ *      $Id: RTMLContact.java,v 1.1 2004-03-12 10:55:18 je Exp $
+ *     $Log: not supported by cvs2svn $
+ */
+
