2ddbd66a7fce96344d3c3eec3a9e1b479f77f66b
Chris Mottram <C.Mottram@ljmu.ac.uk>
2005-06-08 11:14:01 +0000

    Fixed Documentation. Removed spurious errorString. Fixed printStackTrace for dumpStack and printStackTrace() compatibility.

--- org_estar_rtml/RTMLException.java
+++ org_estar_rtml/RTMLException.java
@@ -1,22 +1,20 @@
 // RTMLException.java
-// $Header: /space/home/eng/cjm/cvs/org_estar_rtml/RTMLException.java,v 1.1 2003-02-24 13:19:56 cjm Exp $
+// $Header: /space/home/eng/cjm/cvs/org_estar_rtml/RTMLException.java,v 1.2 2005-06-08 11:14:01 cjm Exp $
 package org.estar.rtml;
+
+import java.io.*;
 
 /**
  * This class extends Exception. 
  * @author Chris Mottram
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class RTMLException extends Exception
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class
 	 */
-	public final static String RCSID = new String("$Id: RTMLException.java,v 1.1 2003-02-24 13:19:56 cjm Exp $");
-	/**
-	 * The error string supplied to the exception.
-	 */
-	private String errorString = null;
+	public final static String RCSID = new String("$Id: RTMLException.java,v 1.2 2005-06-08 11:14:01 cjm Exp $");
 	/**
 	 * An exception that caused this exception to be generated.
 	 */
@@ -29,29 +27,17 @@ public class RTMLException extends Exception
 	public RTMLException(String errorString)
 	{
 		super(errorString);
-		this.errorString = new String(errorString);
 	}
 
 	/**
 	 * Constructor for the exception.
 	 * @param errorString The error string.
-	 * @param exception An exception that caused this exception to be generated.
+	 * @param e An exception that caused this exception to be generated.
 	 */
 	public RTMLException(String errorString,Exception e)
 	{
 		super(errorString);
-		this.errorString = new String(errorString);
 		exception = e;
-	}
-
-	/**
-	 * Retrieve routine for the error string supplied to the exception.
-	 * @return Returns a copy of the errorString for this exception.
-	 * @see #errorString
-	 */
-	public String getErrorString()
-	{
-		return new String(errorString);
 	}
 
 	/**
@@ -65,15 +51,14 @@ public class RTMLException extends Exception
 	}
 
 	/**
-	 * Overridden toString method, that calls super toString and adds errorString to it.
-	 * @see #errorString
+	 * Overridden toString method, that calls super toString and adds exception's toString to it.
 	 */
 	public String toString()
 	{
 		StringBuffer sb = null;
 
 		sb = new StringBuffer();
-		sb.append(errorString);
+		sb.append(super.toString());
 		if(exception != null)
 			sb.append(exception.toString());
 		return sb.toString();
@@ -81,15 +66,32 @@ public class RTMLException extends Exception
 
 	/**
 	 * Overridden printStackTrace, that prints the creating exceptions stack if it is non-null.
+	 * NB Logger.dumpStack uses this method.
+	 * @param s The writer to write to.
 	 */
-	public void printStackTrace()
+	public void printStackTrace(PrintWriter s)
 	{
-		super.printStackTrace();
+		super.printStackTrace(s);
 		if(exception != null)
-			exception.printStackTrace();
+			exception.printStackTrace(s);
+	}
+
+	/**
+	 * Overridden printStackTrace, that prints the creating exceptions stack if it is non-null.
+	 * NB printStackTrace() uses this method.
+	 * @param s The stream to write to.
+	 */
+	public void printStackTrace(PrintStream s)
+	{
+		super.printStackTrace(s);
+		if(exception != null)
+			exception.printStackTrace(s);
 	}
 }
 
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2003/02/24 13:19:56  cjm
+// Initial revision
+//
 //
